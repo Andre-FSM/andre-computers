164c21f16d974458bb587b577168c23046cd9a37
Andre <andreminicz@gmail.com>
2024-03-15 14:14:31 -0300

    first build

---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-JSR52RGY.mjs";import"./chunk-RFKLSGRH.mjs";import"./chunk-G6Q5XAVP.mjs";import"./chunk-I3GRPSGO.mjs";import"./chunk-GTSSCL6B.mjs";import"./chunk-X2D33J2K.mjs";import"./chunk-DOJ2YFHW.mjs";import"./chunk-CQJ2OH7I.mjs";import"./chunk-Z3SDC37A.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
